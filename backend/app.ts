e0c407343df925118098af0144a42ea8d2ce6aa6
Mozarelov1 <denisbubela69@gmail.com>
2025-06-03 22:11:51 +0300

    Update app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -4,32 +4,27 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
-
 import passport from "passport";
-import { AppDataSource } from "./service-auth/config/data-source";
-import passportConfig from "./service-auth/config/passport";
-import authRoutes from "./service-auth/routes/auth-routes";
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+import {AppDataSource} from './service-auth/config/data-source'
 
-dotenv.config({
-  path: path.resolve(__dirname, ".env"),
-});
+const passportConfig = require('./service-auth/config/passport')
+
+const authRoutes = require('./service-auth/routes/auth-routes')
 
 const app = express();
 
-AppDataSource.initialize().catch((err) => {
-  console.error("Error during Data Source initialization:", err);
-});
+dotenv.config({
+    path: path.resolve(__dirname, '.env')
+  });
 
-app.use(helmet());
-app.use(
-  cors({
+app.use(helmet())
+app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
+    origin: `${process.env.CLIENT_URL}` ,
+    
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -37,22 +32,25 @@ passportConfig();
 app.use(passport.initialize());
 
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/auth',authRoutes)
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   if (req.body && typeof req.body.content === 'string') {
+//     req.body.content = xss(req.body.content);
+//   }
+//   next();
+// });
 
 
-app.use("/api/auth", authRoutes);
-
-const start = async () => {
-  try {
-    const port: number = Number(process.env.PORT) || 2000;
-    app.listen(port, () =>
-      console.log(`Server started at http://localhost:${port}`)
-    );
-  } catch (e) {
-    console.error("Error on app start:", e);
-  }
-};
+const start = async () =>{
+        try{
+            AppDataSource.initialize()
+            const port : number = Number(process.env.PORT) || 2000;
+            app.listen(port, () => console.log(`server started at http://localhost:${port}`));
+        }catch(e){
+            console.log(e)
+        }
+    }
 
 start();
 
-export default app;
+module.exports = app;
