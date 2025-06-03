3a965f4c497c86f9ff3dc2d04804da313fca1592
Mozarelov1 <denisbubela69@gmail.com>
2025-06-03 21:14:29 +0300

    auth service

--- backend/app.ts
+++ backend/app.ts
@@ -1,27 +1,38 @@
 import path from "path";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import express from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import xss from 'xss';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import passport from "passport";
+
+import {AppDataSource} from './service-auth/config/data-source'
+
+const passportConfig = require('./service-auth/config/passport')
+
+const authRoutes = require('./service-auth/routes/auth-routes')
 
 const app = express();
 
-// dotenv.config({
-//     path: path.resolve(__dirname, '.env')
-//   });
+dotenv.config({
+    path: path.resolve(__dirname, '.env')
+  });
 
-
-const port : number = Number(process.env.PORT) || 2000;
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors({
-//     credentials: true,
-//     origin: `${process.env.CLIENT_URL}` ,
+app.use(helmet())
+app.use(cors({
+    credentials: true,
+    origin: `${process.env.CLIENT_URL}` ,
     
-// }));
+}));
 
+app.use(express.json());
+app.use(cookieParser());
+
+passportConfig();
+app.use(passport.initialize());
+
+
+app.use('/api/auth',authRoutes)
 // app.use((req: Request, res: Response, next: NextFunction) => {
 //   if (req.body && typeof req.body.content === 'string') {
 //     req.body.content = xss(req.body.content);
@@ -30,11 +41,10 @@ const port : number = Number(process.env.PORT) || 2000;
 // });
 
 
-
-
-
 const start = async () =>{
         try{
+            AppDataSource.initialize()
+            const port : number = Number(process.env.PORT) || 2000;
             app.listen(port, () => console.log(`server started at http://localhost:${port}`));
         }catch(e){
             console.log(e)
@@ -42,3 +52,5 @@ const start = async () =>{
     }
 
 start();
+
+module.exports = app;
